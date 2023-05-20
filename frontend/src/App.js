6f0dbf6593665312300677fb1610fc1865d666d0
huuzey <weleladinsefa@gmail.com>
2023-05-20 14:36:52 +0300

    base url modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import "./index.css";
-export const BASE_URL = "https://huze-tour-travel.onrender.com";
+export const BASE_URL = "https://huze-tour-travel.onrender.com/api/v1";
 function App() {
   return (
     <>
